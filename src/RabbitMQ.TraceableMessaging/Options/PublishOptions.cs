d92402d3ac24d904b1b6127d80b626670b7414e3
Dmitriy Larionov <d.lar@outlook.com>
2019-04-15 09:39:40 +0300

    Fix to PublishOptions to avoid ambiguity of contructor.

--- src/RabbitMQ.TraceableMessaging/Options/PublishOptions.cs
+++ src/RabbitMQ.TraceableMessaging/Options/PublishOptions.cs
@@ -3,8 +3,8 @@ namespace RabbitMQ.TraceableMessaging.Options
     public class PublishOptions
     {
         public string Exchange { get; set; } = "";
-        public string RoutingKey { get; set; }
+        public string RoutingKey { get; set; } = "";
         public PublishOptions() { }
-        public PublishOptions(string routingKey = "") => RoutingKey = routingKey;
+        public PublishOptions(string routingKey) => RoutingKey = routingKey;
     }
 }
